b11b870e2ca0c8f441ac31e2329ef01987b1a3ae
Olga Boriskina <boriskina1992@gmail.com>
2017-11-12 12:59:10 +0300

    update drag_and_drop

--- drag_and_drop/drag_and_drop.py
+++ drag_and_drop/drag_and_drop.py
@@ -9,18 +9,14 @@ drag_locator = 'id("draggable")'
 drop_locator = 'id("droppable")'
 
 
-class DragAndDrop:
-    def __init__(self, driver):
-        self._driver = driver
-        self._wait = WebDriverWait(self._driver, 10)
+def switch_to_frame(driver, wait):
+    frame = wait.until(expected.element_to_be_clickable((By.XPATH, frame_locator)))
+    driver.switch_to.frame(frame)
 
-    def switch_to_frame(self):
-        frame = self._wait.until(expected.element_to_be_clickable((By.XPATH, frame_locator)))
-        self._driver.switch_to.frame(frame)
 
-    def drag_and_drop(self):
-        self.switch_to_frame()
-        source_element = self._wait.until(expected.element_to_be_clickable((By.XPATH, drag_locator)))
-        dest_element = self._wait.until(expected.element_to_be_clickable((By.XPATH, drop_locator)))
-        ActionChains(self._driver).drag_and_drop(source_element, dest_element).perform()
-        return dest_element.text
+def move(driver, wait):
+    switch_to_frame(driver, wait)
+    source_element = wait.until(expected.element_to_be_clickable((By.XPATH, drag_locator)))
+    dest_element = wait.until(expected.element_to_be_clickable((By.XPATH, drop_locator)))
+    ActionChains(driver).drag_and_drop(source_element, dest_element).perform()
+    return dest_element.text
